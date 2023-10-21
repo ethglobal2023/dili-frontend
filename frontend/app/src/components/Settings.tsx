9bf667986282bd9e54448a68fcbdb3ed64c870e1
A A <adoll@As-MacBook-Pro.local>
2023-10-21 03:47:24 -0400

    Basic, but functioning settings. Not wired up to a real backend, for demonstration only

--- frontend/app/src/components/Settings.tsx
+++ frontend/app/src/components/Settings.tsx
@@ -1,6 +1,6 @@
 import {Controller, useFieldArray, useForm} from 'react-hook-form';
-import {Select, SelectItem, SelectContent, SelectTrigger, SelectValue} from "../../@/components/ui/select";
-import {AiFillAlert, AiOutlineAccountBook} from "react-icons/ai";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "../../@/components/ui/select";
+import {AiOutlineAccountBook, AiOutlineAntDesign, AiOutlineBell} from "react-icons/ai";
 import {Input} from "../../@/components/ui/input";
 import {Button} from "../../@/components/ui/button";
 import {Label} from "../../@/components/ui/label";
@@ -9,12 +9,26 @@ interface FormValues {
     searchEngine: string;
     ipfsGateway: string;
     minTrustScore: number;
+    minGitcoinScore: number;
     trustedIssuers: { address: string; label: string; color: string }[];
 }
 
 export const Settings = () => {
-    const {handleSubmit, control, register} = useForm<FormValues>();
-    const { fields, append, remove } = useFieldArray({
+    const {handleSubmit, control, register} = useForm<FormValues>({
+        defaultValues: {
+            searchEngine: "https://dili.com/search",
+            ipfsGateway: "https://dili.com/ipfs",
+            minTrustScore: 10,
+            trustedIssuers: [
+                {
+                    address: "0x9cbC225B9d08502d231a6d8c8FF0Cc66aDcc2A4F",
+                    label: "DILI EAS Signer",
+                    color: "#40e0d0"
+                }
+            ]
+        }
+    });
+    const {fields, append, remove} = useFieldArray({
         control,
         name: "trustedIssuers"
     });
@@ -25,77 +39,106 @@ export const Settings = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={"p-4 space-y-4"}>
-            <Label htmlFor="searchEngine">Search engine</Label>
-            <Select {...register("searchEngine")}  autoComplete="on">
-                <SelectTrigger>
-                    <SelectValue placeholder="Search backend"/>
-                </SelectTrigger>
-                <SelectContent id={"searchEngine"}>
-                    <SelectItem value="https://backend.com">
-                        <AiOutlineAccountBook name="placeholder-icon"/> DILI - https://backend.com
-                    </SelectItem>
-                    <SelectItem value="dark">
-                        <AiFillAlert name="placeholder-icon"/> TalentLayer - https://backen2d.com
-                    </SelectItem>
-                    <SelectItem value="system">
-                        <AiFillAlert name="placeholder-icon"/> DILI3 - https://backen2d.com
-                    </SelectItem>
-                </SelectContent>
-            </Select>
-            <Label htmlFor="minTrustScore">Min trust score for new connections</Label>
-            <Input {...register("minTrustScore")} id={"minTrustScore"} type="number" placeholder={"Min trust score for new connection"}/>
-
-            <Label htmlFor="ipfsGateway">IPFS Gateway</Label>
-            <Select {...register("ipfsGateway")}  autoComplete="on">
-                <SelectTrigger>
-                    <SelectValue placeholder="Search backend"/>
-                </SelectTrigger>
-                <SelectContent id={"ipfsGateway"}>
-                    <SelectItem value="https://backend.com">
-                        <AiOutlineAccountBook name="placeholder-icon"/>Cloudflare - https://backend.com
-                    </SelectItem>
-                    <SelectItem value="dark">
-                        <AiFillAlert name="placeholder-icon"/> DILI - https://backen2d.com
-                    </SelectItem>
-                    <SelectItem value="system">
-                        <AiFillAlert name="placeholder-icon"/> web3.storage - https://web3.storage
-                    </SelectItem>
-                </SelectContent>
-            </Select>
-            {fields.map((item, index) => (
-                <fieldset key={item.id}>
-                    <Controller
-                        name={`items[${index}].Wa`}
-                        control={control}
-                        defaultValue={item.address}
-                        render={({ field }) => <Input {...field} />}
-                    />
-                    <Controller
-                        name={`items[${index}].label`}
-                        control={control}
-                        defaultValue={item.label}
-                        render={({ field }) => <input {...field} />}
-                    />
-                    <Controller
-                        name={`items[${index}].color`}
-                        control={control}
-                        defaultValue={item.color}
-                        render={({ field }) => <input type="color" {...field} />}
-                    />
-                    <button type="button" onClick={() => remove(index)}>
-                        Remove
-                    </button>
-                </fieldset>
-            ))}
-            <button
+            <div>
+                <Label htmlFor="searchEngine">Search engine</Label>
+                <Select {...register("searchEngine")} autoComplete="on">
+                    <SelectTrigger>
+                        <SelectValue placeholder="Search backend"/>
+                    </SelectTrigger>
+                    <SelectContent id={"searchEngine"}>
+                        <SelectItem value="https://dili.com/search">
+                            <div className={"flex align-middle space-x-2"}>
+                                <AiOutlineAccountBook name="placeholder-icon"/>
+                                <span>Cloudflare - https://dili.com/search</span>
+                            </div>
+                        </SelectItem>
+                        <SelectItem value="https://www.talentlayer.org">
+                            <div className={"flex align-middle space-x-2"}>
+                                <AiOutlineAccountBook name="placeholder-icon"/>
+                                <span>TalentLayer - https://www.talentlayer.org</span>
+                            </div>
+                        </SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+            <div>
+                <Label htmlFor="minTrustScore">Min trust score for new connections</Label>
+                <Input {...register("minTrustScore")} id={"minTrustScore"} type="number"
+                       placeholder={"Min trust score for new connections"}/>
+            </div>
+            <div>
+                <Label htmlFor="minGitcoinScore">Min GitCoin score for new connections</Label>
+                <Input {...register("minGitcoinScore")} id={"minGitcoinScore"} type="number"
+                       placeholder={"Min GitCoin score for new connections"}/>
+            </div>
+            <div>
+                <Label htmlFor="ipfsGateway">IPFS Gateway</Label>
+                <Select {...register("ipfsGateway")} autoComplete="on">
+                    <SelectTrigger>
+                        <SelectValue placeholder="Search backend"/>
+                    </SelectTrigger>
+                    <SelectContent id={"ipfsGateway"}>
+                        <SelectItem value="https://cloudflare-ipfs.com/ipfs">
+                            <div className={"flex align-middle space-x-2"}>
+                                <AiOutlineAccountBook name="placeholder-icon"/>
+                                <span>Cloudflare - https://cloudflare-ipfs.com/ipfs</span>
+                            </div>
+                        </SelectItem>
+                        <SelectItem value="https://dili.com">
+                            <div className={"flex align-middle space-x-2"}>
+                                <AiOutlineAntDesign name="placeholder-icon"/>
+                                <span>DILI - https://dili.com</span>
+                            </div>
+                        </SelectItem>
+                        <SelectItem value="https://w3s.link/ipfs">
+                            <div className={"flex align-middle space-x-2"}>
+                                <AiOutlineBell name="placeholder-icon"/>
+                                <span>Cloudflare - https://w3s.link/ipfs</span>
+                            </div>
+                        </SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+            <div>
+                <Label htmlFor="trustedIssuers">Trusted issuers</Label>
+                <div id={"trustedIssuers"} className={"flex-col space-y-4"}>
+                    {fields.map((item, index) => (
+                        <fieldset key={item.id} className={"p-4 border-2 border-gray-300"}>
+                            <Label htmlFor={`trustedIssuers.${index}.address`}>Address</Label>
+                            <Controller
+                                name={`trustedIssuers.${index}.address`}
+                                control={control}
+                                defaultValue={item.address}
+                                render={({field}) => <Input {...field} />}
+                            />
+                            <Label htmlFor={`trustedIssuers.${index}.label`}>Friendly Name</Label>
+                            <Controller
+                                name={`trustedIssuers.${index}.label`}
+                                control={control}
+                                defaultValue={item.label}
+                                render={({field}) => <Input {...field} />}
+                            />
+                            <Label htmlFor={`trustedIssuers.${index}.color`}>Label color</Label>
+                            <Controller
+                                name={`trustedIssuers.${index}.color`}
+                                control={control}
+                                defaultValue={item.color}
+                                render={({field}) =>
+                                    <Input type="color" {...field} />}
+                            />
+                            <button type="button" onClick={() => remove(index)}>
+                                Remove
+                            </button>
+                        </fieldset>
+                    ))}
+                </div>
+            </div>
+            <Button
                 type="button"
-                onClick={() => append({ address: '', label: '', color: '#ffffff' })}
+                onClick={() => append({address: '', label: '', color: '#ffffff'})}
             >
                 Add Item
-            </button>
-            <button type="submit">Submit</button>
-
-
+            </Button>
             <Button type="submit">Submit</Button>
         </form>
     );
